7dc547d553ad3aa638d03e531c745c523a45dd24
mayarassem9 <mayarassem9@gmail.com>
2023-06-16 14:44:08 -0700

    betty edits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *a program that prints a line with puts function
+ * main -a program that prints a line with puts function
+ *return: 0 (success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }  
